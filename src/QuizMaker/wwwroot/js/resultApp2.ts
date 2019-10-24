0514375a26e2be7c233e3b4099b19030d7e99714
Janne Mattila <janne.mattila@hotmail.fi>
2019-10-24 22:22:35 +0300

    Initial implementation of Chart.js results page

--- src/QuizMaker/wwwroot/js/resultApp2.ts
+++ src/QuizMaker/wwwroot/js/resultApp2.ts
@@ -1,4 +1,4 @@
-﻿declare var d3: typeof import("d3");
+﻿declare var Chart: typeof import("chart.js");
 declare var signalR: typeof import("@aspnet/signalr");
 import { ConnectionViewModel } from "./quizTypes";
 import { ResultViewModel, ResultQuestionViewModel, ResultQuestionAnswerViewModel } from "./resultTypes";
@@ -36,7 +36,7 @@ connection.on('Results', function (r: ResultViewModel) {
     console.log(r);
 
     results = r;
-    renderQuizResults(results);
+    renderQuizResults(results, false);
 });
 
 connection.onclose(function (e: any) {
@@ -76,82 +76,81 @@ function getQuestionTitle(results: ResultViewModel) {
     return title;
 }
 
-function renderQuizResults(results: ResultViewModel) {
+let labels = new Array<string>();
+let values = new Array<number>();
+let data = {
+    labels: [],
+    datasets: [{
+        backgroundColor: "blue",
+        borderColor: "blue",
+        data: []
+    }]
+};
+
+function renderQuizResults(results: ResultViewModel, forceDraw: boolean) {
 
     let resultsTitleElement = document.getElementById("resultsTitle") as HTMLElement;
     resultsTitleElement.innerText = getQuestionTitle(results);
 
+    data.labels = [];
+    data.datasets[0].data = [];
+
     let resultQuestion = new ResultQuestionViewModel();
     if (results.results.length > 0) {
         resultQuestion = results.results[0];
+        for (let i = 0; i < resultQuestion.answers.length; i++) {
+            const answer = resultQuestion.answers[i];
+            data.labels.push(answer.name);
+            data.datasets[0].data.push(answer.count);
+        }
     }
 
     let responsesElement = document.getElementById("responses");
     responsesElement.innerHTML = `${results.responses} 📝`;
 
-    let svg = d3.select("svg");
-    svg.selectAll("*").remove();
-    let containerElement = document.getElementById("containerElement") as HTMLDivElement;
-    svg.attr("width", Math.min(containerElement.clientWidth, window.innerWidth * 0.8));
-    svg.attr("height", Math.min(containerElement.clientHeight, window.innerHeight * 0.8));
-    let margin = {
-        top: 20,
-        right: 20,
-        bottom: 30,
-        left: 50
-    };
-    let width = +svg.attr("width") - margin.left - margin.right;
-    let height = +svg.attr("height") - margin.top - margin.bottom;
-    let g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    let x = d3.scaleBand()
-        .rangeRound([0, width])
-        .padding(0.1);
+    console.log(labels);
+    console.log(forceDraw);
+    console.log(data);
 
-    let y = d3.scaleLinear()
-        .rangeRound([height, 0]);
+    if (forceDraw) {
+        let canvas = <HTMLCanvasElement>document.getElementById('quizChart');
+        const containerElement = document.getElementById("containerElement") as HTMLDivElement;
+        canvas.width = Math.min(containerElement.clientWidth, window.innerWidth * 0.8);
+        canvas.height = Math.min(containerElement.clientHeight, window.innerHeight * 0.8);
 
-    x.domain(resultQuestion.answers.map(function (d: ResultQuestionAnswerViewModel) {
-        return d.name;
-    }));
-    y.domain([0, d3.max(resultQuestion.answers, function (d: ResultQuestionAnswerViewModel) {
-        return Number(d.count);
-    })]);
+        const context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .style("font-size", "16px")
-        .call(d3.axisBottom(x))
+        var config = {
+            type: 'bar',
+            data: data,
+            options: {
+                responsive: false,
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            suggestedMin: 0,
+                            suggestedMax: 5,
+                            stepSize: 1
+                        }
+                    }]
+                }
+            }
+        };
 
-    g.append("g")
-        .call(d3.axisLeft(y))
-        .style("font-size", "16px")
-        .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Count");
-
-    g.selectAll(".quiz-results-bar")
-        .data(resultQuestion.answers)
-        .enter().append("rect")
-        .attr("class", "quiz-results-bar")
-        .attr("x", function (d: any) {
-            return x(d.name);
-        })
-        .attr("y", function (d: any) {
-            return y(Number(d.count));
-        })
-        .attr("width", x.bandwidth())
-        .attr("height", function (d: any) {
-            return height - y(Number(d.count));
-        });
+        (<any>window).resultChart = new Chart.Chart(context, config);
+    }
+    else {
+        (<any>window).resultChart.update();
+    }
 }
 
 window.addEventListener('resize', () => {
     console.log("resize");
-    renderQuizResults(results);
+    renderQuizResults(results, true);
 });
 
-renderQuizResults(results);
+renderQuizResults(results, true);
