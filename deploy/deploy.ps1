2033257aa7a58e6909d8105c3bd0916e76bfdf5e
Janne Mattila <janne.mattila@hotmail.fi>
2019-10-25 16:21:05 +0300

    Change default name

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,6 +1,6 @@
 Param (
     [Parameter(HelpMessage="Deployment target resource group")] 
-    [string] $ResourceGroupName = "quizmaker-local-rg",
+    [string] $ResourceGroupName = "quiz-local-rg",
 
     [Parameter(HelpMessage="Deployment target resource group location")] 
     [string] $Location = "North Europe",
